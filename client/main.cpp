c6dd63652b3cdfaa282c158ed379a93de0bbf023
Sergeydigl3-PC <26508358+Sergeydigl3@users.noreply.github.com>
2024-03-07 17:21:12 +0300

    1st small example of UDP communication

--- client/main.cpp
+++ client/main.cpp
@@ -6,38 +6,29 @@
 #include "main.h"
 
 
-#include <boost/asio.hpp>
 #include <iostream>
-#include <thread>
+#include <boost/asio.hpp>
 
 using boost::asio::ip::udp;
 
 int main() {
     try {
-        boost::asio::io_service io_service;
+        boost::asio::io_context io_context;
+        udp::resolver resolver(io_context);
+        udp::endpoint receiver_endpoint = *resolver.resolve(udp::v4(), "localhost", "12345").begin();
 
-        udp::socket socket(io_service, udp::endpoint(udp::v4(), 0));
+        udp::socket socket(io_context);
+        socket.open(udp::v4());
 
-        udp::resolver resolver(io_service);
-        udp::endpoint endpoint = *resolver.resolve({udp::v4(), "localhost", "daytime"});
-        socket.connect(endpoint);
+        std::string message = "Ping";
+        socket.send_to(boost::asio::buffer(message), receiver_endpoint);
 
-        for (;;) {
-            std::array<char, 1> send_buf  = {{ 0 }};
-            socket.send(boost::asio::buffer(send_buf));
-
-            std::array<char, 128> recv_buf;
-            udp::endpoint sender_endpoint;
-            size_t len = socket.receive_from(boost::asio::buffer(recv_buf), sender_endpoint);
-
-            std::cout.write(recv_buf.data(), len);
-
-            std::this_thread::sleep_for(std::chrono::seconds(5));
-        }
+        std::array<char, 128> recv_buffer;
+        udp::endpoint sender_endpoint;
+        size_t len = socket.receive_from(boost::asio::buffer(recv_buffer), sender_endpoint);
+        std::cout.write(recv_buffer.data(), len);
+    } catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
     }
-    catch (std::exception& e) {
-        std::cerr << e.what() << std::endl;
-    }
-
     return 0;
 }
--- server/main.cpp
+++ server/main.cpp
@@ -1,44 +1,38 @@
 //
 // Created by Serge on 05.03.2024.
 //
+
 #include <iostream>
 #include <boost/asio.hpp>
-#include <boost/algorithm/string/case_conv.hpp>
 
 using boost::asio::ip::udp;
 
-std::string make_daytime_string() {
-    // Возвращает текущее время в формате дня и времени
-    // (например, "Thu Mar 07 12:35:15 2024")
-    // Это можно заменить на свою логику формирования сообщения.
-    // В данном примере используется текущее системное время.
-    time_t now = time(0);
-    std::cout << "Got request" << std::endl;
-    return ctime(&now);
+std::string make_pong_message() {
+    return "Pong";
 }
 
 int main() {
     try {
         boost::asio::io_context io_context;
 
-        // Создаем объект ip::udp::socket для приема запросов на порту 13 (Daytime Protocol).
-        udp::socket socket(io_context, udp::endpoint(udp::v4(), 8888));
+        // Создаем UDP сокет для приема запросов на порту 9876
+        udp::socket socket(io_context, udp::endpoint(udp::v4(), 12345));
 
         for (;;) {
-            std::array<char, 1> recv_buf;
-            udp::endpoint remote_endpoint;
+            std::array<char, 4> recv_buf; // Буфер для принимаемых данных
+            udp::endpoint remote_endpoint; // Информация об удаленном клиенте
 
-            // Ожидаем, пока клиент свяжется с нами.
+            // Принимаем данные от клиента
             socket.receive_from(boost::asio::buffer(recv_buf), remote_endpoint);
+            std::cout << "Получен запрос от " << remote_endpoint.address() << ":" << remote_endpoint.port() << std::endl;
+            // Определяем, что будем отправлять обратно клиенту
+            std::string response = make_pong_message();
 
-            // Определяем, что мы будем отправлять обратно клиенту.
-            std::string message = make_daytime_string();
-
-            // Отправляем ответ на удаленный адрес.
-            socket.send_to(boost::asio::buffer(message), remote_endpoint);
+            // Отправляем ответ обратно на адрес клиента
+            socket.send_to(boost::asio::buffer(response), remote_endpoint);
         }
     } catch (std::exception& e) {
-        std::cerr << e.what() << std::endl;
+        std::cerr << "Ошибка: " << e.what() << std::endl;
     }
 
     return 0;
